e6d6dd7b8bc71caeb547c3cd5f34bcf1e6d0f616
SamidhaManjrekar <smanjrekar135@gmail.com>
2025-02-09 20:38:53 +0530

    urls modification

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api/', include('itinerary.urls')),
     path('api/', include('blog.urls')), 
+    path('api/', include('budget.urls')), 
 ] 
 
 if settings.DEBUG:
