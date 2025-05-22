b228281a4361bd4621db2091cb7459f2e2fa5544
SamidhaManjrekar <smanjrekar135@gmail.com>
2025-05-22 20:30:39 +0530

    hash

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import LandingPage from './pages/Landing/LandingPage'
 import Signin from './pages/Authentication/Signin';
 import Signup from './pages/Authentication/Signup';
