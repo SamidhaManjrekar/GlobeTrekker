c0440016bb2278d2e93335babf1ff2fd537590a2
SamidhaManjrekar <smanjrekar135@gmail.com>
2025-06-16 23:36:47 +0400

    updated vite.config

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/GlobeTrekker/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 })
