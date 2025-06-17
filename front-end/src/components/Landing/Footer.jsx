97b94f5c516673849e51b8199fab923c00fd1617
SamidhaManjrekar <smanjrekar135@gmail.com>
2025-06-17 17:38:39 +0400

    imagekit debugging

--- front-end/src/components/Landing/Footer.jsx
+++ front-end/src/components/Landing/Footer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom"; 
 
 const Footer = () => {
   return (
     <footer className="p-4 mt-10 bg-[var(--clr-gold-dark)]">
       <div className="mx-auto">
         <div className="text-center">
-          <a href="#" className="font-montserrat font-semibold text-lg">
+          <Link to="/" className="font-montserrat font-semibold text-lg">
             Globe Trekker
-          </a>
+          </Link>
           <p className="mt-1 text-sm">
             Explore the world with us. Your journey begins here.
           </p>
