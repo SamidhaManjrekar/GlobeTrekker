9a695db4ea6015305bb545b00b7de9ad63c2ae48
SamidhaManjrekar <smanjrekar135@gmail.com>
2025-07-23 17:35:44 +0530

    interceptor update

--- front-end/src/api/interceptor.js
+++ front-end/src/api/interceptor.js
@@ -2,20 +2,31 @@ import axios from 'axios';
 import { ACCESS_TOKEN } from '@/constants/access';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+    baseURL: import.meta.env.VITE_API_URL, 
+});
 
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem(ACCESS_TOKEN);
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`
+
+        const publicEndpoints = [
+            '/api/signup/',
+            '/api/signin/',
+            '/api/token/', 
+            '/api/token/refresh/', 
+        ];
+        
+        const isPublicEndpoint = publicEndpoints.some(endpoint => config.url.startsWith(endpoint));
+        
+        if (token && !isPublicEndpoint) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
-        return config
+        
+        return config;
     },
     (error) => {
-        return Promise.reject(error)
+        return Promise.reject(error);
     }
-)
+);
 
-export default api
+export default api;
